85d641b0ab9da49883e8d1cf34151e662d2d11ec
M-yadav007 <76170376+M-yadav007@users.noreply.github.com>
2023-02-28 03:31:54 +0530

    Time: 3 ms (63.86%), Space: 7.5 MB (61.69%) - LeetHub

--- 0024-swap-nodes-in-pairs/0024-swap-nodes-in-pairs.cpp
+++ 0024-swap-nodes-in-pairs/0024-swap-nodes-in-pairs.cpp
@@ -1,27 +1,50 @@
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode() : val(0), next(nullptr) {}
- *     ListNode(int x) : val(x), next(nullptr) {}
- *     ListNode(int x, ListNode *next) : val(x), next(next) {}
- * };
- */
-class Solution {
-public:
-    ListNode* swapPairs(ListNode* head) {
-        if (!head || !head->next) return head;
-        ListNode *dummyNode = new ListNode(0,head);
-        ListNode *curr = head;
-        ListNode *prev = dummyNode;
-        while(curr && curr->next){
-            prev->next = curr->next;
-            curr->next = prev->next->next;
-            prev->next->next = curr;
-            prev = curr;
-            curr = curr->next;
+// /**
+//  * Definition for singly-linked list.
+//  * struct ListNode {
+//  *     int val;
+//  *     ListNode *next;
+//  *     ListNode() : val(0), next(nullptr) {}
+//  *     ListNode(int x) : val(x), next(nullptr) {}
+//  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+//  * };
+//  */
+// class Solution {
+// public:
+//     ListNode* swapPairs(ListNode* head) {
+//         if (!head || !head->next) return head;
+//         ListNode *dummyNode = new ListNode(0,head);
+//         ListNode *curr = head;
+//         ListNode *prev = dummyNode;
+//         while(curr && curr->next){
+//             prev->next = curr->next;
+//             curr->next = prev->next->next;
+//             prev->next->next = curr;
+//             prev = curr;
+//             curr = curr->next;
+//         }
+//         return dummyNode->next;
+//     }
+// };
+class Solution  {
+    public:
+
+ListNode* swapPairs(ListNode* head) {
+        ListNode* temp = new ListNode(0);
+        temp->next = head;
+        ListNode* curr = temp;
+        
+        while(curr->next!= NULL && curr->next->next!= NULL)
+        {
+            ListNode* first = curr->next;
+            ListNode* second = curr->next->next;
+            first->next = second->next;
+            curr->next = second;
+            second->next = first;
+            
+            curr = curr->next->next;
         }
-        return dummyNode->next;
-    }
+        
+        
+        return temp->next;
+}
 };
