529d32a4581b18834ec7757cd032420194e0dc27
M-yadav007 <76170376+M-yadav007@users.noreply.github.com>
2023-03-01 03:29:10 +0530

    Time: 216 ms (92.06%), Space: 114.9 MB (69.65%) - LeetHub

--- 0234-palindrome-linked-list/0234-palindrome-linked-list.cpp
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.cpp
@@ -1,16 +1,81 @@
+// class Solution {
+// public:
+//     ListNode* temp;
+    
+  
+//     bool isPalindrome(ListNode* head) {
+//         ListNode* fast=head;
+//         ListNode* slow=head;
+//         ListNode* check=head;
+
+//         while(fast!=NULL && fast->next!=NULL)
+//         {
+//             fast=fast->next->next;
+//             slow=slow->next;
+//         }
+
+//         while(slow!=NULL)
+//         {
+//             if(check->val!=slow->val)
+//             {
+//                 return false;
+//             }
+//             slow=slow->next;
+            
+//         }
+//     }
+// };
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
 class Solution {
 public:
-    ListNode* temp;
-    
-    bool check(ListNode* head) {
-        if (NULL == head) return true;
-        bool res = check(head->next) & (temp->val == head->val);
-        temp = temp->next;
-        return res;
+    void reverseList(ListNode* &node, ListNode* &prev) {
+        while (node) {
+            ListNode* next = node->next;
+            node->next = prev;
+            prev = node;
+            node = next;
+        }
     }
-    
+    ListNode* getSecondHalfStartNode(ListNode* node) {
+        ListNode* slow = node;
+        ListNode* fast = node->next;
+        while (fast) {
+            if (fast->next && fast->next->next) {
+                slow = slow->next;
+                fast = fast->next->next;
+            }
+            else {
+                if (fast->next) slow = slow->next;
+                break;
+            }
+        }
+        return slow->next;
+    }
     bool isPalindrome(ListNode* head) {
-        temp = head;
-        return check(head);
+        ListNode* secondHalfHead = getSecondHalfStartNode(head);
+        ListNode* prev = NULL;
+        reverseList(secondHalfHead, prev);
+        ListNode* secondHalfHeadRev = prev;
+        ListNode* firstHalfHead = head;
+        while (secondHalfHeadRev) {
+            if (firstHalfHead->val != secondHalfHeadRev->val) {
+                return false;
+            }
+            firstHalfHead = firstHalfHead->next;
+            secondHalfHeadRev = secondHalfHeadRev->next;
+        }
+        ListNode* reversePrev = NULL;
+        reverseList(prev, reversePrev);
+        return true;
     }
 };
